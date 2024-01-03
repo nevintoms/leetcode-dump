41db15a9c6c82d1dfd36d5afbfe8dae6abdb757c
Nevin Toms <nevintoms333@gmail.com>
2024-01-03 13:27:11 -0500

    Add bruteforce method

--- reverse-vowels-of-a-string.py
+++ reverse-vowels-of-a-string.py
@@ -14,3 +14,26 @@ Example 2:
 Input: s = "leetcode"
 Output: "leotcede"
 '''
+
+# First Solution - time O(N), space O(3N)
+def reverseVowels(self, s):
+    """
+    :type s: str
+    :rtype: str
+    """
+    vowel = []
+    ind = []
+    new_str = []
+    for i in range(len(s)):
+        if s[i] in 'aeiouAEIOU':
+            vowel.append(s[i])
+            ind.append(i)
+        else:
+            new_str.append(s[i])
+    
+    vowel_rev = vowel[::-1]
+    print(vowel_rev)
+
+    for i, x in enumerate(ind):
+        new_str.insert(x, vowel_rev[i])
+    return "".join(new_str)
