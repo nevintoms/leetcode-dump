64feb705b91f4d64cd0e664f8e909f697207a300
Nevin Toms <nevintoms333@gmail.com>
2024-01-20 11:47:37 -0500

    Add solution 1

--- move-zeros.py
+++ move-zeros.py
@@ -22,3 +22,26 @@ Constraints:
     1 <= nums.length <= 104
     -231 <= nums[i] <= 231 - 1
 '''
+
+# This question can be thought of moving zeros to the end or moving non zeros to the begining. 
+# Later would be the best approach.
+
+# Solution 1: Moving zeros to the end
+def moveZeroes(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: None Do not return anything, modify nums in-place instead.
+    """
+    i = 0
+    j = 1
+    while((i < len(nums)-1) & (j < len(nums))):
+        if nums[i] == 0:
+            if nums[j]!=0:
+                nums[i], nums[j] = nums[j], nums[i]
+                i+=1
+        else:
+            i+=1
+        j+=1
+    return nums
+
+
